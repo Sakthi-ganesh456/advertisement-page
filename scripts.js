3c675101d3fed3ef2a48031a05e1eb59d75caa23
Sakthi-ganesh456 <sakthiganesh870@gmail.com>
2024-08-25 14:05:57 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById('shopping-cart icon').addEventListener('click', function() {
+    window.location.href = 'https://amzn.in/d/16Wptkq'; // Redirect to the homepage or shopping page
+});
